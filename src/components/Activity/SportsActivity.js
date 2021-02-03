8447fb1ce37c97e43aef0a7bd3101864f2b1ed4c
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-03 03:31:54 +0100

    getRequest werkt

---
+++ src/components/Activity/SportsActivity.js
@@ -0,0 +1,44 @@
+import React, {useState, useEffect} from "react";
+import axios from "../../services/axios.instance";
+import authHeader from "../../services/auth-header";
+
+
+
+
+function SportsActivity() {
+
+   const [activity, setActivity] = useState([])
+
+    const trainerId = authHeader().id;
+
+
+    useEffect(()=>{
+        axios.get(`user_sports/trainer/${trainerId}`)
+            .then(response =>(
+                console.log(response.data["activities"]
+                )))
+
+
+
+    }, [])
+
+    useEffect(()=>{
+                 axios.get(`user_sports/trainer/`)
+                    .then(response =>(
+                       setActivity(response.data
+                       )))
+
+
+
+    }, [])
+
+
+
+    return(
+     <>
+         {activity}
+     </>
+    );
+}
+
+export default SportsActivity;
