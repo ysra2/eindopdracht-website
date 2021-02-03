b7465ec9e69e63fd269234f1810dfe56cbfeb491
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-03 05:09:52 +0100

    werkt alleen past zich niet aan

---
+++ src/components/Activity/UpdateActivity.js
@@ -0,0 +1,114 @@
+import React, {useEffect, useState} from "react"
+import axios from "../../services/axios.instance"
+import authHeader from "../../services/auth-header";
+import AddActivity from "./AddActivity";
+import authActivity from "../../services/auth-activity";
+import AuthService from "../../services/auth.service";
+
+
+const UpdateActivity = (props) => {
+    const [updateActivity, setUpdateActivity] = useState({
+        activityName: "",
+        nameTrainer: "",
+        location: "",
+        date: "",
+        time: ""});
+
+    const activityChange = event => {
+        const {id, value} = event.target;
+        (setUpdateActivity({
+            ...updateActivity,
+            [id]: value
+        }));
+    };
+
+
+    const creatActivity = event => {
+        event.preventDefault();
+
+        // const activityId = AuthService.getCurrentActivity().activityId
+        const activityId = authActivity().activityId
+        console.log(activityId);
+        return axios.put(`activity/${activityId} `,
+            updateActivity)
+            .then(
+                (response) => {
+                    console.log(response);
+                })
+            .then(
+                props.history.push('/activiteit')
+            )
+            .catch((error) => {
+                console.log(error);
+            });
+
+
+        // const trainerId = authHeader().id;
+        // const activityId = authActivity().activityId
+        // console.log(activityId);
+        // return axios.put(`activity/${activityId} `,
+        //     updateActivity)
+        //     .then(
+        //         (response) => {
+        //             console.log(response);
+        //         })
+        //     .then((result) => {
+        //         if (activityId === activityId) {
+        //             props.history.push('/activiteit')
+        //         }
+        //         console.log(result)
+        //     })
+        //     .then(
+        //         props.history.push('/activiteit')
+        //     )
+        //     .catch((error) => {
+        //         console.log(error);
+        //     });
+
+
+    }
+
+
+
+    return(
+        <div>
+
+            <form/>
+            <div>
+                <label>Sportactiviteit</label>
+                <input id="activityName"  placeholder="Sportactiviteit"
+                       value={updateActivity.activityName} onChange={activityChange}/>
+
+            </div>
+            <div>
+                <label>Naam Trainer</label>
+                <input id="nameTrainer" placeholder="Naam Trainer"
+                       value={updateActivity.nameTrainer} onChange={activityChange}/>
+
+            </div>
+            <div>
+                <label>Locatie</label>
+                <input id="location" type="address" placeholder="Locatie"
+                       value={updateActivity.location} onChange={activityChange}/>
+
+            </div>
+            <div>
+                <label>Tijd</label>
+                <input id="time" type="time" placeholder="Tijd"
+                       value={updateActivity.time} onChange={activityChange}/>
+
+            </div>
+            <div>
+                <label>Datum</label>
+                <input id="date" type="date" placeholder="Datum"
+                       value={updateActivity.date} onChange={activityChange}/>
+
+            </div>
+            <button onClick={creatActivity}>
+                Wijzig sportactiviteit
+            </button>
+        </div>
+    )
+}
+
+export default UpdateActivity;
