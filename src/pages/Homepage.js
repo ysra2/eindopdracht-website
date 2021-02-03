112e1653252c8191d142b777f0fd35d9254977c3
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-03 03:27:53 +0100

    map toegevoegd

--- src/pages/Homepage.js
+++ src/pages/Homepage.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Maps from "../components/Maps";
 
 function Homepage() {
     return(
         <div >
             <header>
             </header>
+            <Maps/>
         </div>
     )
 
