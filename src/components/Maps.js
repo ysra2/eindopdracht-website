24c9b2510c143aef251942ffd38f39f0bb11dc6d
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-10 05:43:11 +0100

    Map aangepast+styling

--- src/components/Maps.js
+++ src/components/Maps.js
@@ -1,178 +1,167 @@
+//https://www.youtube.com/watch?v=WZcxJGmLbSo
+
 import React, {useCallback, useRef, useState} from 'react';
 import { GoogleMap, useLoadScript, Marker, InfoWindow,} from "@react-google-maps/api";
-import usePlacesAutocomplete, {getGeocode } from "use-places-autocomplete";
-import ActivityProfile from "./activity/ActivityProfile";
+import usePlacesAutocomplete, { getGeocode, getLatLng,} from "use-places-autocomplete";
+import {Combobox, ComboboxInput, ComboboxPopover, ComboboxList, ComboboxOption} from "@reach/combobox";
+import { formatRelative } from "date-fns";
 
+import mapStyles from "../mapStyles";
+import "@reach/combobox/styles.css";
 
-// import mapStyles from "./mapStyles"
+//door het hier te plaats zorgt het ervoor dat de array niet steeds opnieuw wordt bekeken als object
+//tijdens rerender
+const libraries = ["places"];
 
 const mapContainerStyle = {
-    // width: "100%" ,
-    height: "70vh",
+    height: "100vh",
+    width: "100vw",
 };
-
 const eindhoven = {
-    lat: 51.4381,
-    lng: 5.4752,
+    lat: 51.441643,
+    lng: 5.469722
 };
 const options = {
-    // styles: mapStyles,
-    disableDefaultUI: true, // extra features als van de kaart verwijderd
+    styles: mapStyles,
+    disableDefaultUI: true, // extra features van de kaart verwijderd
     zoomControl: true, //in en uitzoomen behouden
 };
 
 export default function Maps() {
-    const {isLoaded, LoadError} = useLoadScript({
+    const {isLoaded, loadError} = useLoadScript({
         googleMapsApiKey: "AIzaSyAeL0sH-SHddn-Dq_m0k0vTcuYLEzS2X5M",
+        libraries
     });
+
+    //(State gebruik als je wilt dat react gererenderd wordt)
+
+    //Plaats een marker
     const [markers, setMarkers] = useState([]);
+    //Laat informatie van de marker
     const [selected, setSelected] = useState(null);
 
-    const onMapClick = useCallback((e) =>{
-        setMarkers(current => [...current, {
-            lat: e.latLng.lat(), //calls an event
-            lng: e.latLng.lng(),
-            time: new Date(),
-        },
+    const onMapClick = useCallback((event) =>{
+        setMarkers((current) => [
+            ...current,
+            {
+                lat: event.latLng.lat(),
+                lng: event.latLng.lng(),
+                time: new Date(),
+            },
         ]);
     }, []);
 
+    //geeft ons een veranderd variable terug
+    //de huidige eigenschap zal dan geïnitialiseerd worden
+    //teruggegeven object zal op zijn plaats blijven staan (tot levensduur van de component)
+    //Deze functie zorgt er dus voor dat de marker op de kaart gepinned blijft.
     const mapRef = useRef();
-    const onMapLoad = useCallback((map) => {
-        mapRef.current = map;
-    }, []);
 
-    const panTo = useCallback(({lat, lng}) => {
+    // deze functie zorgt ervoor dat als de map geladen is, het de map teruggeeft dat
+    // ervoor zorgt dat we gebruik kunnen maken van de mapRef om zo rerendering te voorkomen.
+    const onMapLoad = useCallback((map) =>{
+        mapRef.current = map;
+    },[]);
+
+    const panTo = useCallback(({lat, lng}) =>{
         mapRef.current.panTo({lat, lng});
-        // mapRef.current.setZoom(15);
+        mapRef.current.setZoom(16);
     }, [])
 
-    if (LoadError) return "Error Loading maps";
-    if (!isLoaded) return "Loading Maps";
+    if(loadError) return "Error loading Maps";
+    if(!isLoaded) return  "Loading Maps";
+
 
     return (
         <div>
-            <Search/>
-            <meta/> <GoogleMap
-            id="map"
+            <GoogleMap
             mapContainerStyle={mapContainerStyle}
-            zoom={12.7}
+            zoom={12}
             center={eindhoven}
             options={options}
             onClick={onMapClick}
             onLoad={onMapLoad}
-        >
-            {markers.map((marker) => (
-                <Marker
-                    // Funtcie voor PT om locatie te kiezen
-                    //  icon is een optie https://www.youtube.com/watch?v=WZcxJGmLbSo zie vanaf 16:30
-                    key={`${marker.lat}-${marker.lng}`}
+            >
+                    <h1>Train Fast</h1>
+                    <Search panTo={panTo}/>
+
+                {markers.map((marker) =>(
+                    <Marker
+                    key={marker.time.toISOString}
                     position={{lat: marker.lat, lng: marker.lng}}
-                    //infoscherm dat gepopped wordt als je op een marker drukt
                     onClick={() => {
                         setSelected(marker);
                     }}
                 />
-            ))}
-
-            {selected ?
-                (<InfoWindow
-                        position={{lat: selected.lat, lng: selected.lng}}
-                        onCloseClick={() => {setSelected(null);}}>
-                        {/*<div style={{ width: '18rem' }}>*/}
-                        {/*        <div>Boksen</div>*/}
-                        {/*        <div >Hendrik DunantPark, Eindhoven</div>*/}
-                        {/*        <div>Tijdstip: 12:00</div>*/}
-                        {/*        <div>Datum: 22-11-2020</div>*/}
-                        {/*        <button>Meld je hier aan</button>*/}
-                        {/*</div>*/}
-                        <ActivityProfile/>
-                    </InfoWindow>
-                ) : null}
-        </GoogleMap>
+                ))}
+                {selected ? (<InfoWindow
+                position={{lat: selected.lat, lng: selected.lng}}
+                onCloseClick={()=>{
+                    setSelected(null);
+                }}>
+                    <div>
+                        <h2>Sportactiviteit</h2>
+                        <p>Activiteit: Boksen</p>
+                        <p>Tijdstip: {formatRelative(selected.time, new Date())}</p>
+                    </div>
+                </InfoWindow>) : null}
+            </GoogleMap>
         </div>
-    );
+    )
 }
 
-function Locate({ panTo }) {
-    return (
-        <button
-            className="locate"
-            onClick={() => {
-                navigator.geolocation.getCurrentPosition(
-                    (position) => {
-                        panTo({
-                            lat: position.coords.latitude,
-                            lng: position.coords.longitude,
-                        });
-                    },
-                    () => null
-                );
-            }}
-        >
-
-        </button>
-    );
-}
-
-
-
-function Search({ panTo }) {
-    const {
-        ready,
+function Search({panTo}) {
+    const {ready,
         value,
-        suggestions: { status, data },
+        suggestions: {status, data},
         setValue,
-        clearSuggestions
-    } = usePlacesAutocomplete({
+        clearSuggestions } = usePlacesAutocomplete({
         requestOptions: {
-            location: { lat: () => 51.4381, lng: () => 5.4752 },
-            radius: 100 * 1000,
-        },
+            location: {lat: () => 51.441643, lng: () => 5.469722},
+            radius: 100*1000,
+
+        }
     });
-
-    const handleInput = (e) => {
-        setValue(e.target.value);
-    };
-
 
     return (
         <div className="search">
-            <div>
-                onSelect ={async (address) => {
-                    try {
-                        const results = await getGeocode({address});
-                        console.log(results.map((entry) =>{
-                            return (
-                                <li key={entry.formatted_address}>
-                                    {entry.formatted_address}
-                                </li>
-                            );
-                        }));
-                    } catch (error) {
-                        console.log("Error");
-                    }
-
-                }}>
-                <input
-                    value={value}
-                    onChange={handleInput}
-                    disabled={!ready}
-                    placeholder="Zoek je locatie"
+            <Combobox onSelect={ async (address) => {
+                // als er op een suggestie geklikt wordt dan laat het de informatie zien zie graag
+                // terug willen ontvangen.
+                setValue(address, false);
+                //dit zorgt ervoor dat gegevens niet gefetched worden uit de google data
+                //dit is een functie dat onderdeel is van usePlacesAutocomplete
+                clearSuggestions();
+                //clearSuggestions laat niet alle suggesties zien maar een deel wat opgevraagd wordt
+                try {
+                    const results = await getGeocode({address});
+                    // adress wordt opgevraagd dat door de functie geocode een resultaat aanroept van het address
+                    // dat weer de lat,lng opvraagt en weergeeft als de locatie is gepinned
+                    const {lat, lng} = await getLatLng(results[0]);
+                    panTo({lat, lng});
+                }catch (error) {
+                    console.log(error)
+                    
+                }}}>
+            {/*//2 childern zullen in de combobox geplaatst worden*/}
+                <ComboboxInput value={value}
+                               onChange={(event) => {
+                                   setValue(event.target.value);
+                               }}
+                               disabled={!ready}
+                               placeholder="Zoek locatie"
                 />
-                <div>
-                    <div>
+                <ComboboxPopover>
+                    <ComboboxList>
                         {status === "OK" &&
                         data.map(({ id, description }) => (
-                            <div key={id} value={description} />
+                            <ComboboxOption key={id} value={description} />
                         ))}
-                    </div>
-                </div>
-            </div>
-
-        </div>
-    );
+                    </ComboboxList>
+                </ComboboxPopover>
+            </Combobox>
+        </div>)
 }
 
 
-//https://www.youtube.com/watch?v=WZcxJGmLbSo
+
