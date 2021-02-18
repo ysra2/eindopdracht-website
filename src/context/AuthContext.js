9dbb8f3045556980294a89a3a375254a32913932
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-18 04:05:30 +0100

    context toegevoegd

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,35 +1,37 @@
-// import React, {useEffect, useState} from "react";
-// import {createContext} from "istanbul-lib-report";
-//
-// //Houdt in de context bij of we:
-// //gebruikersdata hebben
-// //of de gebruikers geautoriseerd zijn
-//
-// const AuthContext = createContext({});
-// //children property zorgt ervoor dat je deze authcontext om je app kunt plaatsen om zo de authorizatie te controleren
-// function AuthContextProvider ({ children }) {
-//     const [authState, setAuthState] = useState({
-//         //Om de status op pending te zetten zorg je ervoor dat de applicatie gaat wachten
-//         // totdat het genoeg informatie heeft verzameld
-//         status: "pending",
-//         error: null,
-//         user: null,
-//
-//     })
-//     //useEffect is een lifecycle method wat ervoor zorgt dat wij kunnen aangeven wanneer het iets moet verrichten.
-//     //in de useEffect kun je in de anonieme functie effect: aangeven wat er moet gebeuren en
-//     // bij de dependency array deps: zorg je ervoor dat het geen loop veroorzaakt.
-//     useEffect(() => {}, []);
-//
-//
-//     return(
-//         <AuthContext.Provider>
-//             {children}
-//         </AuthContext.Provider>
-//     );
-// }
-//
-// export {
-//     AuthContext,
-//     AuthContextProvider
-// }
+import React, { createContext, useContext, useEffect, useState } from 'react';
+const AuthContext = createContext({});
+
+function AuthContextProvider({ children }) {
+    const [authState, setAuthState] = useState({
+        status: 'pending',
+        error: null,
+        user: null,
+    })
+    useEffect(() => {
+        // haal uit de local storage de JWT Token
+        // Als die er niet is, kunnen we gewoon verder
+        // Als die token er wel is, dan betekend dat dat de applicatie herstart is
+        // En dan willen we nog even onze gebruikersdata (username, etc.) ophalen.
+        setTimeout(() => {
+            // er is geen token, dus we beginnen met een schone lei!
+            setAuthState({
+                ...authState,
+
+                status: 'done',
+            })
+        }, 2000)
+    }, []);
+
+    return (
+        <AuthContext.Provider value={{authState}}>
+            {authState.status === 'done' && children}
+            {authState.status === 'pending' && <p>Loading...</p>}
+        </AuthContext.Provider>
+    );
+}
+
+
+export {
+    AuthContext,
+    AuthContextProvider,
+}
