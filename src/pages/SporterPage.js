280029447b86662d57df6459736b1e60b0885c92
Yousra Khayati <y.khayati@novi-eduction.nl>
2021-02-24 18:21:47 +0100

    Opzet aanpassen

--- src/pages/SporterPage.js
+++ src/pages/SporterPage.js
@@ -1,21 +1,30 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import Maps from "../components/Maps";
+import Maps from "../components/map/Maps";
 
 export default function SporterPage() {
     return (
-        <div >
-            <div >
-                <nav >
-                    <ul>
-                        <li><Link to="/list">Activiteiten</Link></li>
-                    </ul>
-                </nav>
+        <>
+            <div className="form">
+                <h4>
+                    Hier vind u een overzicht van alle sportactiviteiten in Eindhoven
+                </h4>
+                <Link to="/list">
+                    <button>Bekijk Activiteiten</button>
+                </Link>
+
+
+                <div>
+                <h5 >
+                    Door op de Marker van de Kaart te drukken ziet u welke sportactiviteit plaatsvind en waar het gelokaliseerd is.
+                    Kunt vanuit de kaart meteen aanmelden voor een sportactiviteit.
+
+                </h5>
+                    <Maps/>
+                </div>
             </div>
-            <div className="map">
-                <Maps/>
-            </div>
-        </div>
+
+        </>
     )
 
 }
--- src/pages/TrainerPage.js
+++ src/pages/TrainerPage.js
@@ -1,29 +1,15 @@
-import React, {useEffect, useState} from 'react';
-import UpdateActivity from "../components/activity/UpdateActivity";
-import AddActivity from "../components/activity/AddActivity";
-import Maps from "../components/Maps";
-import {Link} from "react-router-dom";
-import ListActivity from "../components/activity/ListActivity";
+import React from 'react';
+import Footer from "../components/Footer";
+import Activity from "../components/sportsActivity/Activity";
+
 
 function TrainerPage() {
     return(
-        <div >
-            <div >
-                <nav >
-                    <ul>
-                        <li><Link to="/post">Activiteiten</Link></li>
-                        <li><Link to="/update">Wijzig Training</Link></li>
-                    </ul>
-                </nav>
-            </div>
-            <div >
-                <AddActivity/>
-            </div>
+        <>
             <div>
-                <Maps/>
+                <div><Activity/></div>
             </div>
-            {/*<AddActivity/>*/}
-        </div>
+        </>
     )
 }
 export default TrainerPage;
